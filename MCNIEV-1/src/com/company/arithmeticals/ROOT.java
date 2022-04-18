404620f02db648cd4b471996f129068b0e79628d
Roy-McNiev <88599586+Roy-McNiev@users.noreply.github.com>
2022-04-18 12:37:50 +0800

    Delete MCNIEV-1/src/com/company/arithmeticals directory

--- MCNIEV-1/src/com/company/arithmeticals/ROOT.java
+++
@@ -1,7 +0,0 @@
-package com.company.arithmeticals;
-
-public class ROOT {
-    public static double solve(Double target, Double v) {
-        return Math.pow(target, 1.0 / v);
-    }
-}
--- MCNIEV-1/src/com/company/arithmeticals/SQRT.java
+++
@@ -1,7 +0,0 @@
-package com.company.arithmeticals;
-
-public class SQRT {
-    public static double solve(Double target) {
-        return ROOT.solve(target, 2.0);
-    }
-}
